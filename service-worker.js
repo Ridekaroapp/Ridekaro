a3fde537ff5bab29448617b5144ac1b97ab48a7a
Ridekaroapp <singharyan3506@gmail.com>
2026-04-25 16:50:24 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,26 @@
-self.addEventListener('install', function(e) {
-  console.log('Service Worker Installed');
+const CACHE_NAME = "ridekaro-cache-v1";
+
+const urlsToCache = [
+  "/Ridekaro/",
+  "/Ridekaro/index.html",
+  "/Ridekaro/logo.png",
+  "/Ridekaro/manifest.json"
+];
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
+  );
 });
